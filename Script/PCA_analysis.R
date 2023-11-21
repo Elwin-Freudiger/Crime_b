bf14f1062d5916af56223d31ecb7d99e2f1b5b61
Elwin Freudiger <elwin.freudiger@gmail.com>
2023-11-21 14:00:10 +0100

    PCA analysis

--- Script/PCA_analysis.R
+++ Script/PCA_analysis.R
@@ -6,50 +6,67 @@ library(corrplot)
 library(tidyverse)
 library(ggplot2)
 library(plotly)
+library(ggcorrplot)
+library(factoextra)
+install.packages("ggfortify")
+library(ggfortify)
 
 #Let's run a PCA analysis
 
 #Load our data first.
 PCA_rate <- read_csv(here::here("data_end/PCA_rate.csv"))
 
-colnames(PCA_rate) <- c("Dep_name",
-                        "Other assault and battery",
-                        "House burglary",
-                        "Assault and battery",
-                        "Domestic assault and battery",
-                        "Destruction and damage",
-                        "Drug trafficking",
-                        "Drug use",
-                        "Sexual violence",
-                        "Theft with weapons", 
-                        "Theft of vehicle accessories",
-                        "Theft from vehicles", 
-                        "Vehicle theft",
-                        "Non-violent theft against individuals",
-                        "Violent theft without weapon")
+#colnames(PCA_rate) <- c("Dep_name",
+                        #"Other assault and battery",
+                        #"House burglary",
+                        #"Assault and battery",
+                        #"Domestic assault and battery",
+                        #"Destruction and damage",
+                        #"Drug trafficking",
+                        #"Drug use",
+                        #"Sexual violence",
+                        #"Theft with weapons", 
+                        #"Theft of vehicle accessories",
+                        #"Theft from vehicles", 
+                        #"Vehicle theft",
+                        #"Non-violent theft against individuals",
+                        #"Violent theft without weapon")
 
 #exclude row names
 PCA_nums <-  PCA_rate |>
-  filter(!Dep_name %in% c("75", "92", "93", "94")) |>
-  select(-Dep_name)
+  filter(!Departement %in% c("75", "93", "92", "93", "94")) |>
+  select(-Departement) |>
+  select("Cambriolages de logement", 
+         "Coups et blessures volontaires", 
+         "Destructions et dégradations volontaires", 
+         "Usage de stupéfiants",
+         "Vols sans violence contre des personnes")
 
 
 
-#Compute the correlation matrix first
-cor_matrix <- cor(PCA_nums)
-#plot it
-cor_plot_pca <- corrplot(cor_matrix, tl.pos='l')
 
+#plot the correlation matrix
+corr_matrix <- cor(PCA_normalized)
+ggcorrplot(corr_matrix)
 
-#Run the PCA analysis
-PCA1 <- princomp(PCA_nums)
-summary(PCA1)
+PCA_normalized <- scale(PCA_nums)
 
-PCA_scale<- prcomp(PCA_nums, scale. = TRUE)
-plot(PCA_scale)
+PCA_prin <- prcomp(PCA_normalized)
+PCA_prin
 
-biplot(PCA_scale)
-summary(PCA(PCA_nums))
+#scree plot
+fviz_eig(PCA_prin, addlabels = TRUE)
+
+#biplot
+pca_var_plot <- fviz_pca_var(PCA_prin, col.var = "black")
+pca_var_plot
+
+autoplot(PCA_prin)
+
+d.factanal <- factanal(PCA_normalized, factors = 2, scores = 'regression', rotation = "varimax")
+
+autoplot(d.factanal, label = TRUE, label.size = 3,
+         loadings = TRUE, loadings.label = TRUE, loadings.label.size  = 3)
 
 
 
